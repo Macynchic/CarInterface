a6850fe546fb78233d975aca7b0ba75be2fc8058
FilatovMD <fil123@max.com>
2022-12-19 17:00:02 +0300

    Добавил коментарии на английском языке

--- Interface/Interface.cpp
+++ Interface/Interface.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 using namespace std;
 
-//Тут просто описаны интерфейсы, это не интересно
+//It's just external interfaces, according to the condition of the problem
 class ICar
 {
 public:
@@ -66,60 +66,60 @@ protected:
 
     bool isComplete = false;
 };
-//Конец описания интерфейсов
+//End of interface description
 
 
-/* Так, начнем с того, что интерфейсы в данной задаче используются довольно специфическим образом
-   если быть точнее, то тут неправильно спроектиованы интерфейсы - их состояния (атрибуты) находятся в состоянии protected, НО!
-   реализация интерфейсов по условиям задачи указана как public. Тогда появляется вопрос - зачем нам нужны атрибуты зашитые в интерфейсы
-   ответ - нахрен не нужны. просто уточнение того, что должно быть в классе, который реализует интерфейс */
+/* So, let's start with the fact that interfaces in this problem are used in a rather specific way.
+    to be more precise, the interfaces are designed incorrectly here - their states (attributes) are in the protected state, BUT!
+    the implementation of interfaces according to the conditions of the task is specified as public. Then the question arises - why do we need attributes wired into interfaces
+    The answer is - fuck not needed. just a clarification of what should be in a class that implements an interface */
 
 class Engine : public IEngine
 {
 
 public:
       
-/*Создадим конструктор, который будет принимать в себя указатель на объект Интерфейса.Указатель на суперкласс является указателем на все его расширения
-  сюда мы будем пподключать наш топливный бак. Отсюдая пошел проектировать топливный бак (иди к топливному баку) */
+/*Let's create a constructor that will take a pointer to an Interface object. A pointer to a superclass is a pointer to all its extensions
+   here we will connect our fuel tank. From here I went to design the fuel tank (go to the fuel tank)*/
 
-    Engine(IFuelTank *pfuelTank)                    //Конструктор для движка
+    Engine(IFuelTank *pfuelTank)                    //engine constructor
     {
         fuelTank = pfuelTank;
     }
 
     IFuelTank* fuelTank;
 
-    void Consume(double liters) override            //Этот метод посылает сообщение подключенному к двигателю топливному баку. 
-                                                    //Вызывая его, вызываем такой-же у подключенного топливного бака
+    void Consume(double liters) override            //This method sends a message to the fuel tank connected to the engine.
+                                                    //Calling it, we call the same one for the connected fuel tank
     {
         fuelTank->Consume(liters); 
     }
-/* Вот тут начинается заковыка. Объясняю. По идее, двигатель как объект должен получасть все состояния топливного бака. И перед тем как запуститься
-двигатель должен проводить проверку атрибутов топливного бака на его состояние. Но это невозможно используя указатель на интерфейс в котором, вроде как,
-и храняться состояния, потому что они спрятаны в модификатор private. Таким образом проверку состояния я буду осуществлять через объект класса
-FuelTankDisplay уже непосредственно в объекте Car. Как бы машина будет анализировать состояние и топливного бака и двигателя через Дисплей*/
+/* This is where the trick begins. I explain. In theory, the engine as an object should receive all the states of the fuel tank. And before you run
+the engine must carry out an attribute check of the fuel tank for its condition. But this is not possible using an interface pointer in which, sort of,
+and store states because they are hidden in the private modifier. Thus, I will check the state through a class object
+FuelTankDisplay is already directly in the Car object. How would the machine analyze the status of both the fuel tank and the engine through the display*/
     void Start() override                          
     {
         
-        setIsRunning(true);                         //Поэтому тут просто перевод состояния двигателя в включенный
+        setIsRunning(true);                         //Therefore, here it is simply a transfer of the engine state to on
         
     }
 
     void Stop() override
     {
-        setIsRunning(false);                        //Или выключеный
+        setIsRunning(false);                        //Or off
     }
 
 
-    bool getIsRunning()                             //Гетер состояния двигателя
+    bool getIsRunning()                             //Engine status geter
     {
         return isRunning;
     }
 
 protected:
     
-    void setIsRunning(bool state)                   //Сетер состояния двигателя. Спрячем ото всех.
-    {                                               //Идем к FuelTankDisplay
+    void setIsRunning(bool state)                   //Engine status setter. Let's hide from everyone
+    {                                               //Going to FuelTankDisplay
         isRunning = state;
     }
     
@@ -130,61 +130,61 @@ protected:
 class FuelTank : public IFuelTank
 {
    
-    friend class FuelTankDisplay;           //Объявим класс FuelTankDisplay дружественным для того, чтобы у дисплея был доступ к реальному 
-                                            //состоянию топливного бака
+    friend class FuelTankDisplay;           //Let's declare the class FuelTankDisplay friendly so that the display has access to the real
+                                            //fuel tank condition
     
 public:
 
-    void Consume(double liters) override    //Дальше просто. вот тут расход топлива за цикл
+    void Consume(double liters) override    //Further simply. here is the fuel consumption per cycle
     {
         if (fillLevel > 0)
         {
             fillLevel -= liters;
         }
         
-        if (fillLevel < 5)                  //Если уровень топлива ниже пяти литров, загореться лампочке  isOnReserve  
+        if (fillLevel < 5)                  //If the fuel level is below five liters, the isOnReserve light will turn on. 
         {
             isOnReserve = true;
         }
 
-        isComplete = false;                 //Как только запустится двигатель, объем топлива будет точно ниже чем максимальный уже на первом цикле. 
-                                            //Тогда погаснет лампочка isComplete
+        isComplete = false;                 //As soon as the engine starts, the fuel volume will be exactly lower than the maximum already on the first cycle.
+                                            //Then the isComplete light will turn off.
 
     }
 
-    void Refuel(double liters) override     //Тут идет заправка машины
+    void Refuel(double liters) override     //Here comes the refueling
     {
         
-        fillLevel += liters;                //Доливаем топливо в бак
+        fillLevel += liters;                //Adding fuel to the tank
         
-        if (fillLevel > 5)                  //Проверяем состояние лампочек. Если топлива больше чем 5 литров, то isOnReserve погасла
+        if (fillLevel > 5)                  //Check the condition of the light bulbs. If there is more than 5 liters of fuel, then isOnReserve goes out
         {
             isOnReserve = false;
         }
 
-        if (fillLevel >= 60)                //Проверяем состояние лампочек. Если топлива больше или равно 60 литров, то isOnReserve погасла
+        if (fillLevel >= 60)                //Check the condition of the light bulbs. If the fuel is greater than or equal to 60 liters, then isOnReserve is extinguished
         {
             isComplete = true;
         }                                   
-        else                                //На всякий случай - погасить лампочку isComplete если бак не полный
+        else                                //Just in case - turn off the isComplete light if the tank is not full
         {
             isComplete = false;
         }
     }
 
-    void setFillLevel(double liters)        //Вот тут сеттер для установки начального уровня топлива при создании машины
+    void setFillLevel(double liters)        //Here is a setter for setting the initial fuel level when creating a car
     {
-        fillLevel = liters;                 //хардкод для задания значения объема бака
+        fillLevel = liters;                 //hardcode for setting the value of the tank volume
         
-        if (fillLevel < 5)                  //И сразу проверяем состояние (атрибуты) нашего топливного бака по входным параметрам значения fillLevel
+        if (fillLevel < 5)                  //And immediately check the state (attributes) of our fuel tank according to the input parameters of the fillLevel value
         {
             isOnReserve = true;
         }
-        if (fillLevel > 5)                  //Если больше пятерки isOnReserve не горит, меньше - горит
+        if (fillLevel > 5)                  //If more than five isOnReserve is off, less is on
         {
             isOnReserve = false;
         }
-        if (liters == 60)                   //Если старт - максимальный объем то зажжем isComlete
+        if (liters == 60)                   //If the start is the maximum volume, then fire isComlete
         {
             isComplete = true;
         }
@@ -192,7 +192,7 @@ public:
 
 protected:
 
-    double fillLevel = 0.0;                 //Состояния будущих объектов. Возвращаемся назад к двигателю
+    double fillLevel = 0.0;                 //States of future objects. Back to the engine
 
     bool isOnReserve = false;
 
@@ -203,7 +203,7 @@ class FuelTankDisplay : public IFuelTankDisplay
 {
 protected:
 
-    FuelTank* myFuelTank;                               //Берем указатель на объект класса FuelTank и прячем от пользователя
+    FuelTank* myFuelTank;                               //We take a pointer to an object of the FuelTank class and hide it from the user
 
 public:
         
@@ -212,7 +212,7 @@ public:
         myFuelTank = currentFuelTank;
     }
 
-    double getFillLevel()                               //Дальше все просто. Просто выкидываем атрибуты подключенного топливного бака через объект дисплея
+    double getFillLevel()                               //Then everything is simple. We simply throw out the attributes of the connected fuel tank through the display object
     { 
         return round((myFuelTank->fillLevel)*10)/10;
     }
@@ -231,7 +231,7 @@ public:
 
 protected:
 
-    double fillLevel = 0.0;                             //Атрибуты класса
+    double fillLevel = 0.0;                             //class attributes
 
     bool isOnReserve = false;
 
@@ -242,20 +242,20 @@ class Car : public ICar
 {
 protected:
 
-    FuelTank* fuelTank = new FuelTank;                  //Создаем обычный топливный бак
-    Engine* engine = new Engine(fuelTank);              //Создаем двигатель. Подключаем к нему наш топливный бак
+    FuelTank* fuelTank = new FuelTank;                  //Create a regular fuel tank
+    Engine* engine = new Engine(fuelTank);              //We create an engine. We connect our fuel tank to it
 
 public:
     
-    FuelTankDisplay* fuelTankDisplay = new FuelTankDisplay(fuelTank);       //Создаем дисплей. Подключаем к нему топливный бак
+    FuelTankDisplay* fuelTankDisplay = new FuelTankDisplay(fuelTank);       //We create a display. We connect the fuel tank to it
 
-    Car()                                                                   //Конструктор дефолтный. Создает бибику с 20 литрами топляка в баке
+    Car()                                                                   //Default constructor. Creates car with 20 liters of fuel in the tank
     {
         fuelTank->setFillLevel(20);
     }
     
     
-    Car(double liters)                                                      //Конструктор недефолтный. Создает бибику с заданным кол-вом топляка
+    Car(double liters)                                                      //The constructor is non-default. Creates a car with the given number of fuel
     {
         if (liters > 0)
         {
@@ -267,15 +267,15 @@ public:
         }
     }
 
-    ~Car()                                                           //Деструктор. Когда уничтожаем бибику - чистим за собой движок, топливный бак и дисплей
+    ~Car()                                                           //Destructor. When we destroy car, we clean the engine, fuel tank and display
     {
         delete engine;
         delete fuelTankDisplay;
         delete fuelTank;      
     }
     
-    void EngineStart() override                                      //Завести двигатель. Если топляк больше нуля - запускаем двигатель. Передаем состояние 
-                                                                     //двигателя в engineIsRunning машины
+    void EngineStart() override                                      //Start the engine. If the fuel is greater than zero, we start the engine. Passing state 
+                                                                     //engine in engineIsRunning machine
     {
         if (fuelTankDisplay->getFillLevel() > 0)
         {
@@ -284,47 +284,47 @@ public:
         }
     }
     
-    void EngineStop() override                                        //Остановить двигатель. Состояние двигателя передать в engineIsRunning машины
+    void EngineStop() override                                        //Stop engine. Engine state to pass to engineIsRunning of the machine
     {
         engine->Stop();
         engineIsRunning = engine->getIsRunning();
     }
 
-    void Refuel(double liters) override                             //Дозаправка
+    void Refuel(double liters) override                             //Refueling
     {
-        if (liters > 0)                                             //Проверка что не заливаем отрицательное значение
+        if (liters > 0)                                             //Checking that we do not fill in a negative value
         {
             engine->fuelTank->Refuel(liters);
         }
        
 
-        if (fuelTankDisplay->getFillLevel() > 60)                   //Защита от перелива. Если больше чем 60 значит мы залили полный бак
+        if (fuelTankDisplay->getFillLevel() > 60)                   //Overflow protection. If more than 60, then we filled a full tank
         {
             fuelTank->setFillLevel(60);
         }
     }
 
-    void RunningIdle() override                                     //Собственно работа самого двигателя
+    void RunningIdle() override                                     //The actual operation of the engine itself
     {
-        if (engine->getIsRunning())                                 //Проверка что двигатель запущен
+        if (engine->getIsRunning())                                 //Checking that the engine is running
         {
-            engine->Consume(0.0003);                                //Если запущен, то откачка топлива по 0.0003 за цикл
+            engine->Consume(0.0003);                                //If running, then pumping fuel at 0.0003 per cycle
         }
-        if (fuelTankDisplay->getFillLevel() <= 0)                   //Как только топливо на нуле - выключить двигатель
+        if (fuelTankDisplay->getFillLevel() <= 0)                   //As soon as the fuel is at zero - turn off the engine
         {
             EngineStop();
         }
         
     }
 
-    bool getEngineIsRunning()                                       //Гетер для получения состояния двигателя
+    bool getEngineIsRunning()                                       //Geter for getting the state of the engine
     {
         return engineIsRunning;                                 
     }
 
 protected:
     
-    bool engineIsRunning = false;                                   //Состояние машины
+    bool engineIsRunning = false;                                   //Car condition
 };
 
 
